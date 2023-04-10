cd71db7c2cf3652bc5eab2dc33acb68d6efed071
see2et <seezet1024@gmail.com>
2023-04-10 10:06:22 +0900

    ブロックが動くように

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use std::{thread, time};
+
+const FIELD_WIDTH: usize = 11 + 2;
+const FIELD_HEIGHT: usize = 20 + 1;
+type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+
+#[derive(Clone, Copy)]
+enum BlockKind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
+
+type BlockShape = [[usize; 4]; 4];
+const BLOCKS: [BlockShape; 7] = [
+    // Iブロック
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
+    // Oブロック
+    [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+    // Sブロック
+    [[0, 0, 0, 0], [0, 1, 1, 0], [1, 1, 0, 0], [0, 0, 0, 0]],
+    // Zブロック
+    [[0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+    // Jブロック
+    [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+    // Lブロック
+    [[0, 0, 0, 0], [0, 0, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+    // Tブロック
+    [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+];
+
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if field[y + pos.y + 1][x + pos.x] & BLOCKS[block as usize][y][x] == 1 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn main() {
     let field = [
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -23,15 +74,41 @@ fn main() {
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ];
 
-    // draw a field
-    for y in 0..21 {
-        for x in 0..13 {
-            if field[y][x] == 1 {
-                print!("[]")
-            } else {
-                print!(" .")
+    let mut pos = Position { x: 4, y: 0 };
+
+    // Clear Display
+    println!("\x1b[2J\x1b[H\x1b[?25l");
+
+    for _ in 0..30 {
+        let mut field_buf = field;
+        if !is_collision(&field, &pos, BlockKind::I) {
+            pos.y += 1;
+        }
+        for y in 0..4 {
+            for x in 0..4 {
+                if BLOCKS[BlockKind::I as usize][y][x] == 1 {
+                    field_buf[y + pos.y][x + pos.x] = 1;
+                }
             }
         }
-        println!();
+
+        println!("\x1b[H"); // カーソルを先頭に移動
+
+        // draw a field
+        for y in 0..FIELD_HEIGHT {
+            for x in 0..FIELD_WIDTH {
+                if field_buf[y][x] == 1 {
+                    print!("[]")
+                } else {
+                    print!(" .")
+                }
+            }
+            println!();
+        }
+
+        thread::sleep(time::Duration::from_millis(1000));
     }
+
+    // カーソルを再表示
+    println!("\x1b[?25h");
 }
